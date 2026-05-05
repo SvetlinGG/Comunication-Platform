d2748bb377c30e53fe4e8d91877293ae5488f561
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-05-05 22:17:27 +0200

    authentication service of user

--- client/src/app/core/services/auth.service.ts
+++ client/src/app/core/services/auth.service.ts
@@ -1,7 +1,40 @@
-import { Injectable } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core';
+import { AuthUser } from '../models/user.model';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 @Injectable({ providedIn: 'root'})
 export class AuthService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:27017/api/comunicaton-platform';
+
+  user = signal<AuthUser | null>(this.getUserFromStorage());
+  isLoggedIn = computed(() => !!this.user());
+
+
+  constructor(private http: HttpClient) { }
+
+  register( data: {username: string, email: string, password: string}){
+    return this.http.post<AuthUser>(`${this.apiUrl}/register`, data).pipe(
+      tap((user) => {
+        localStorage.setItem('user', JSON.stringify(user));
+        this.user.set(user)
+      })
+    );
+  }
+
+  login( data: { email: string, password: string}){
+    return this.http.post<AuthUser>(`${this.apiUrl}/login`, data).pipe(
+      tap((user) => {
+        localStorage.setItem('user', JSON.stringify(user));
+        this.user.set(user)
+      })
+    );
+  }
+  
+
+  private getUserFromStorage(): AuthUser | null{
+    const rawUser = localStorage.getItem('user');
+    return rawUser ? JSON.parse(rawUser) : null;
+  }
 }
